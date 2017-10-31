816cf6bdc95a05e0d67b1fbe16a0337408d36de3
Jimish Fotariya <fotariyajimish@gmail.com>
2017-11-01 00:20:18 +0530

    net & comm

---
+++ 197/files/w15/trialtrans.ino
@@ -0,0 +1,40 @@
+#include<SoftwareSerial.h>
+
+int trigPin = 3;
+int echoPin = 2;
+int rxPin = 1;
+int txPin = 0;
+int duration, cm , inches ;
+SoftwareSerial myserial(rxPin, txPin);
+
+void setup() {
+  pinMode(trigPin, OUTPUT) ;
+  pinMode(echoPin, INPUT) ;
+  myserial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  pinMode(echoPin, INPUT);
+  duration = pulseIn(echoPin, HIGH);
+
+  cm = (duration / 2) / 29.1;
+  inches = (duration / 2) / 74;
+
+  if (cm > 0 && cm < 10) {
+    myserial.write('0');
+  }
+  else if ( cm >= 10 && cm < 20) {
+    myserial.write('1');
+  }
+  else
+  { myserial.write('2');
+  }
+
+  //delay(250);
+
+}
